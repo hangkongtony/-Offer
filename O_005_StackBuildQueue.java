f33c374f82db2ea048891b82bab7759e3f33da63
hangkongtony <hangkongtony@gmail.com>
2016-08-30 10:59:24 +0800

    Create O_005_StackBuildQueue.java

---
+++ O_005_StackBuildQueue.java
@@ -0,0 +1,31 @@
+package jizhiOffer;
+import java.util.Stack;
+
+
+public class O_005_StackBuildQueue {
+
+	 Stack<Integer> stack1 = new Stack<Integer>();
+	 Stack<Integer> stack2 = new Stack<Integer>();
+	    
+	    public void push(int node) {
+	        while(!stack2.isEmpty())
+	        {
+	        	stack1.push(stack2.pop());
+	        }
+	        stack1.push(node);
+	    }
+	    
+	    public int pop() {
+	    	while(!stack1.isEmpty())
+	    	{
+	    		stack2.push(stack1.pop());
+	    	}
+	    	return stack2.pop();
+	    }
+	    
+	    
+	public static void main(String[] args) {
+
+	}
+
+}
